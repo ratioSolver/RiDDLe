75e23e16c22f691a7890c48bcce11d169c0d3053
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2022-10-29 09:13:47 +0200

    improved coverage..

---
+++ tests/test_riddle.cpp
@@ -0,0 +1,20 @@
+#include "riddle_parser.h"
+#include <fstream>
+
+using namespace riddle;
+
+int main(int argc, char const *argv[])
+{
+    // the problem files..
+    std::vector<std::string> prob_names;
+    for (int i = 1; i < argc - 1; i++)
+        prob_names.push_back(argv[i]);
+
+    for (const auto &f : prob_names)
+        if (std::ifstream ifs(f); ifs)
+        {
+            parser prs(ifs);
+            auto cu = prs.parse();
+        }
+    return 0;
+}
