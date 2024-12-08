64c84707d580558d1eb7b46d1acf55f473d6e09a
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-12-08 15:04:02 +0100

    Add local_field_statement class to handle local field declarations and execution

--- include/statement.hpp
+++ include/statement.hpp
@@ -11,4 +11,17 @@ namespace riddle
 
     virtual void execute(const scope &scp, env &ctx) const = 0;
   };
+
+  class local_field_statement final : public statement
+  {
+  public:
+    local_field_statement(std::vector<id_token> &&field_type, std::vector<id_token> &&names, std::vector<std::unique_ptr<expression>> &&values) : field_type(std::move(field_type)), names(std::move(names)), values(std::move(values)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    std::vector<id_token> field_type;
+    std::vector<id_token> names;
+    std::vector<std::unique_ptr<expression>> values;
+  };
 } // namespace riddle
