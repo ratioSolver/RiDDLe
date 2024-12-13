f12a86ded5e1e7c1dd42891e140d6385064a144e
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2024-12-13 10:10:23 +0100

    Add various statement classes to enhance statement handling and execution

--- include/statement.hpp
+++ include/statement.hpp
@@ -24,4 +24,91 @@ namespace riddle
     std::vector<id_token> names;
     std::vector<std::unique_ptr<expression>> values;
   };
+
+  class assignment_statement final : public statement
+  {
+  public:
+    assignment_statement(std::vector<id_token> &&object_id, id_token &&field_id, std::unique_ptr<expression> value) : object_id(std::move(object_id)), field_id(std::move(field_id)), value(std::move(value)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    std::vector<id_token> object_id;
+    id_token field_id;
+    std::unique_ptr<expression> value;
+  };
+
+  class expression_statement final : public statement
+  {
+  public:
+    expression_statement(std::unique_ptr<expression> xpr) : xpr(std::move(xpr)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    std::unique_ptr<expression> xpr;
+  };
+
+  class conjunction_statement final : public statement
+  {
+  public:
+    conjunction_statement(std::vector<std::unique_ptr<statement>> &&stmts, std::unique_ptr<expression> cst = nullptr) : stmts(std::move(stmts)), cst(std::move(cst)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    std::vector<std::unique_ptr<statement>> stmts;
+    std::unique_ptr<expression> cst;
+  };
+
+  class disjunction_statement final : public statement
+  {
+  public:
+    disjunction_statement(std::vector<std::unique_ptr<conjunction_statement>> &&blocks) : blocks(std::move(blocks)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    std::vector<std::unique_ptr<conjunction_statement>> blocks;
+  };
+
+  class for_all_statement final : public statement
+  {
+  public:
+    for_all_statement(std::vector<id_token> &&enum_type, id_token &&enum_id, std::vector<std::unique_ptr<statement>> stmts) : enum_type(std::move(enum_type)), enum_id(std::move(enum_id)), stmts(std::move(stmts)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    std::vector<id_token> enum_type;
+    id_token enum_id;
+    std::vector<std::unique_ptr<statement>> stmts;
+  };
+
+  class return_statement final : public statement
+  {
+  public:
+    return_statement(std::unique_ptr<expression> xpr) : xpr(std::move(xpr)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    std::unique_ptr<expression> xpr;
+  };
+
+  class formula_statement final : public statement
+  {
+  public:
+    formula_statement(bool is_fact, id_token &&id, std::vector<id_token> &&tau, id_token &&predicate_name, std::vector<id_token> &&args, std::vector<std::unique_ptr<expression>> &&values) : is_fact(is_fact), id(std::move(id)), tau(std::move(tau)), predicate_name(std::move(predicate_name)), args(std::move(args)), values(std::move(values)) {}
+
+    void execute(const scope &scp, env &ctx) const override;
+
+  private:
+    bool is_fact;
+    id_token id;
+    std::vector<id_token> tau;
+    id_token predicate_name;
+    std::vector<id_token> args;
+    std::vector<std::unique_ptr<expression>> values;
+  };
 } // namespace riddle
