f1964c26f7b0c7c95d14f4f16748d9fc0b2e0a1d
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-03-03 12:12:03 +0100

    Fix missing newline at end of file in examples.rs

--- src/env.rs
+++ src/env.rs
@@ -1,4 +1,4 @@
-use crate::language::{ClassDef, EnumDef, Expr, MethodDef, PredicateDef, ProblemDef};
+use crate::language::{ClassDef, EnumDef, Expr, MethodDef, PredicateDef, ProblemDef, Statement};
 use std::{
     any::Any,
     cell::RefCell,
@@ -127,7 +127,7 @@ pub trait Scope {
     fn parent(&self) -> Option<Rc<dyn Scope>>;
 
     fn get_field(&self, name: &str) -> Option<Rc<Field>>;
-    fn get_method(&self, name: &str) -> Option<Rc<MethodDef>>;
+    fn get_method(&self, name: &str) -> Option<Rc<Method>>;
     fn get_class(&self, name: &str) -> Option<Rc<dyn Class>>;
     fn get_enum(&self, name: &str) -> Option<Rc<EnumDef>>;
     fn get_predicate(&self, name: &str) -> Option<Rc<PredicateDef>>;
@@ -157,7 +157,7 @@ pub struct CommonScope {
     core: Weak<dyn Core>,
     parent: Option<Rc<dyn Scope>>,
     fields: RefCell<HashMap<String, Rc<Field>>>,
-    methods: RefCell<HashMap<String, Vec<Rc<MethodDef>>>>,
+    methods: RefCell<HashMap<String, Vec<Rc<Method>>>>,
     classes: RefCell<HashMap<String, Rc<dyn Class>>>,
     enums: RefCell<HashMap<String, Rc<EnumDef>>>,
     predicates: RefCell<HashMap<String, Rc<PredicateDef>>>,
@@ -192,15 +192,15 @@ impl CommonScope {
         }
     }
 
-    pub fn from_class(core: Rc<dyn Core>, parent: Option<Rc<dyn Scope>>, class: ClassDef) -> Self {
-        let scope = Self::new(core, parent);
+    pub fn from_class(core: Rc<dyn Core>, parent: Option<Rc<dyn Scope>>, class: ClassDef) -> Rc<Self> {
+        let scope = Rc::new(Self::new(core.clone(), parent));
         for (field_type, fields) in class.fields {
             for (name, default) in fields {
                 scope.fields.borrow_mut().insert(name.clone(), Rc::new(Field { name, field_type: field_type.clone(), default }));
             }
         }
         for method in class.methods {
-            scope.methods.borrow_mut().entry(method.name.clone()).or_default().push(Rc::new(method));
+            scope.methods.borrow_mut().entry(method.name.clone()).or_default().push(Rc::new(Method::new(core.clone(), Some(scope.clone()), method)));
         }
         for predicate in class.predicates {
             scope.predicates.borrow_mut().insert(predicate.name.clone(), Rc::new(predicate));
@@ -208,15 +208,23 @@ impl CommonScope {
         scope
     }
 
+    pub fn from_method(core: Rc<dyn Core>, parent: Option<Rc<dyn Scope>>, method: MethodDef) -> Self {
+        let scope = Self::new(core, parent);
+        for (arg_type, arg_name) in method.args {
+            scope.fields.borrow_mut().insert(arg_name.clone(), Rc::new(Field { name: arg_name, field_type: arg_type, default: None }));
+        }
+        scope
+    }
+
     pub fn add_problem(&self, problem: ProblemDef) {
         for method in problem.methods {
-            self.methods.borrow_mut().entry(method.name.clone()).or_default().push(Rc::new(method));
+            self.methods.borrow_mut().entry(method.name.clone()).or_default().push(Rc::new(Method::new(self.core.upgrade().unwrap(), Some(self.core.upgrade().unwrap()), method)));
         }
         for predicate in problem.predicates {
             self.predicates.borrow_mut().insert(predicate.name.clone(), Rc::new(predicate));
         }
         for class in problem.classes {
-            self.classes.borrow_mut().insert(class.name.clone(), Rc::new(CompositeClass::new(self.core.upgrade().unwrap(), class)));
+            self.classes.borrow_mut().insert(class.name.clone(), Rc::new(CompositeClass::new(self.core.upgrade().unwrap(), Some(self.core.upgrade().unwrap()), class)));
         }
         for enm in problem.enums {
             self.enums.borrow_mut().insert(enm.name.clone(), Rc::new(enm));
@@ -237,7 +245,7 @@ impl Scope for CommonScope {
         self.fields.borrow().get(name).cloned().or_else(|| self.parent.as_ref()?.get_field(name))
     }
 
-    fn get_method(&self, name: &str) -> Option<Rc<MethodDef>> {
+    fn get_method(&self, name: &str) -> Option<Rc<Method>> {
         self.methods.borrow().get(name).and_then(|ms| ms.first().cloned()).or_else(|| self.parent.as_ref()?.get_method(name))
     }
 
@@ -254,15 +262,73 @@ impl Scope for CommonScope {
     }
 }
 
+pub struct Method {
+    core: Weak<dyn Core>,
+    scope: CommonScope,
+    name: String,
+    return_type: Option<Vec<String>>,
+    args: Vec<(Vec<String>, String)>,
+    statements: Vec<Statement>,
+}
+
+impl Method {
+    pub fn new(core: Rc<dyn Core>, parent: Option<Rc<dyn Scope>>, mut method: MethodDef) -> Self {
+        Self {
+            core: Rc::downgrade(&core),
+            name: std::mem::take(&mut method.name),
+            return_type: std::mem::take(&mut method.return_type),
+            args: std::mem::take(&mut method.args),
+            statements: std::mem::take(&mut method.statements),
+            scope: CommonScope::from_method(core, parent, method),
+        }
+    }
+}
+
+impl Scope for Method {
+    fn core(self: Rc<Self>) -> Rc<dyn Core> {
+        self.core.upgrade().unwrap()
+    }
+
+    fn parent(&self) -> Option<Rc<dyn Scope>> {
+        self.scope.parent.clone()
+    }
+
+    fn get_field(&self, name: &str) -> Option<Rc<Field>> {
+        self.scope.get_field(name)
+    }
+
+    fn get_method(&self, name: &str) -> Option<Rc<Method>> {
+        self.scope.get_method(name)
+    }
+
+    fn get_class(&self, name: &str) -> Option<Rc<dyn Class>> {
+        self.scope.get_class(name)
+    }
+
+    fn get_enum(&self, name: &str) -> Option<Rc<EnumDef>> {
+        self.scope.get_enum(name)
+    }
+
+    fn get_predicate(&self, name: &str) -> Option<Rc<PredicateDef>> {
+        self.scope.get_predicate(name)
+    }
+}
+
 pub struct CompositeClass {
     core: Weak<dyn Core>,
-    name: String,
     scope: CommonScope,
+    name: String,
+    instances: RefCell<Vec<Rc<CompositeObject>>>,
 }
 
 impl CompositeClass {
-    pub fn new(core: Rc<dyn Core>, class: ClassDef) -> Self {
-        Self { core: Rc::downgrade(&core), name: class.name.clone(), scope: CommonScope::new(core, None) }
+    pub fn new(core: Rc<dyn Core>, parent: Option<Rc<dyn Scope>>, class: ClassDef) -> Self {
+        Self {
+            core: Rc::downgrade(&core),
+            scope: CommonScope::new(core, parent),
+            name: class.name.clone(),
+            instances: RefCell::new(Vec::new()),
+        }
     }
 }
 
@@ -276,7 +342,9 @@ impl Class for CompositeClass {
     }
 
     fn new_instance(self: Rc<Self>) -> Rc<dyn Object> {
-        unimplemented!()
+        let instance = Rc::new(CompositeObject::new(self.clone(), None));
+        self.instances.borrow_mut().push(instance.clone());
+        instance
     }
 }
 
@@ -293,7 +361,7 @@ impl Scope for CompositeClass {
         self.scope.get_field(name)
     }
 
-    fn get_method(&self, name: &str) -> Option<Rc<MethodDef>> {
+    fn get_method(&self, name: &str) -> Option<Rc<Method>> {
         self.scope.get_method(name)
     }
 
