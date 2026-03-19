76dbdda9140c0878e02ac7b212b89e0f6aeab87c
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-03-19 10:28:43 +0100

    Refactor Core trait and execute function: update assert method to return void and adjust execute logic to handle assertions without error propagation.

--- src/language.rs
+++ src/language.rs
@@ -218,11 +218,8 @@ pub struct Disjunction {
 pub fn execute(scp: Rc<dyn Scope>, env: Rc<dyn Env>, stmt: &Statement) -> Result<(), RiddleError> {
     match stmt {
         Statement::Expr(expr) => {
-            if scp.clone().core().assert(to_cnf(evaluate(scp.clone(), env.clone(), expr)?)) {
-                Ok(())
-            } else {
-                Err(RiddleError::RuntimeError("Assertion failed".into()))
-            }
+            scp.clone().core().assert(to_cnf(evaluate(scp.clone(), env.clone(), expr)?));
+            Ok(())
         }
         Statement::LocalField { field_type, fields } => {
             let (first, rest) = field_type.split_first().ok_or_else(|| RiddleError::RuntimeError("Empty field type path".into()))?;
